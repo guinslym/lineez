05047099cf0f3ba521be016232bec76ffad066f9
Guinslym <gmond071@pcsun.ad.uottawa.ca>
2016-11-05 21:25:42 -0400

    command

---
+++ applications/places/management/commands/createpost.py
@@ -0,0 +1,4 @@
+from django.core.management.base import BaseCommand, CommandError
+from application.places.models import Poll
+
+#https://docs.djangoproject.com/en/1.10/howto/custom-management-commands/
